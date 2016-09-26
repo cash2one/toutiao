ffacc84d59f460d8eff8247a4b4eb3ab4e2ae469
章鱼 <751231221@qq.com>
2016-09-26 17:25:28 +0800

    新闻头条

---
+++ assets/js/app.js
@@ -0,0 +1,33 @@
+function touAjax(type, key, url) {
+	ajax: $.ajax({
+		type: 'get',
+//		dataType: 'jsonp',
+		url: url,
+		data: {
+			'type': type, //类型
+			'key': key //密钥
+		},
+		headers:{
+			'Access-Control-Allow-Origin': '*'
+
+		},
+		success: function(data) {
+			//			alert('a');
+			//转行成对象
+			//			touObj = JSON.parse(data);
+			console.log(data);
+			//生成html调用
+			//			touList(courseObj.list);
+		},
+		error: function(jqXHR) {
+			console.log('发生错误:' + jqXHR.status);
+		}
+
+	});
+}
+var key = '2d6bf5ead491f6e342129dd69c9d4487',
+	type = ['top', 'shehui', 'guonei', 'guoji', 'yule', 'tiyu', 'junshi', 'keji', 'caijing', 'shishang'],
+	touObj = '',
+	touList = '',
+	url = 'http://v.juhe.cn/toutiao/index';
+touAjax(type, key, url);
